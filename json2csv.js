a5b03f56e98f2589623464d7d2562cae4b8bb501
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-01-06 21:26:47 -0500

    include comments

--- json2csv.js
+++ json2csv.js
@@ -3,11 +3,13 @@
 var csvConverter = {
   convert: function (objArray) {
     var array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    //Assume that the headers of the document are equal to the keys in the JSON object. 
     var headers = Object.keys(array[0]);
     return this.parseHeaders(headers, array);
   },
   
   parseHeaders: function(headers, array) {
+    //Push the headers into the CSV string. 
     var str = '';
     headers.forEach(function(item) {
       str += item + ',';
@@ -29,6 +31,9 @@ var csvConverter = {
           value = item[index];
        
         if (typeof value  === "string") {
+          // If the value contained in the JSON object is a string:
+          // Perform a regex test to check and see if the value has a comma already in place and escape the value. 
+          // e.g. "Smith, Jones" as a value should not be separated two different columns. 
           value = regex.test(value) ? '"' + value + '"' : value;
         }
         line += value;
